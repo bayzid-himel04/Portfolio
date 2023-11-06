a5e4448461dfb0ad041d267ee2ff52bea252defe
bayzid-himel04 <tanvir.porosh@gmail.com>
2023-11-06 23:40:51 +0600

    minor adjustment

--- src/Component/NavBar/NavBar.jsx
+++ src/Component/NavBar/NavBar.jsx
@@ -13,10 +13,10 @@ const NavBar = () => {
             transition={{duration:0.5}}
             >Bayzid Himel</motion.span>
             <div className="social">
-            <a href=''><img src="/facebook.png" alt=""/></a>
-            <a href=''><img src="/instagram.png" alt=""/></a>
-            <a href=''><img src="/github.png" alt=""/></a>
-            <a href=''><img src="/LinkedIn.png" alt=""/></a>
+            <a href=''target="_blank" rel="noopener noreferrer"><img src="/facebook.png" alt=""/></a>
+            <a href=''target="_blank" rel="noopener noreferrer"><img src="/instagram.png" alt=""/></a>
+            <a href='https://github.com/bayzid-himel04' target="_blank" rel="noopener noreferrer"><img src="/github.png" alt=""/></a>
+            <a href='https://www.linkedin.com/in/bayzid-himel-98228a18b/'target="_blank" rel="noopener noreferrer"><img src="/LinkedIn.png" alt=""/></a>
             </div>
         </div>
     </div>
